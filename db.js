b97163823ef7ee18118686504609d0a2e0bbe10d
Sylvain <sylvainterwilly@gmail.com>
2022-09-15 14:17:48 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,9 @@
+const db = require('./data/db.json');
+const products = require('./data/products.json');
+const signupUsers = require('./data/signupUsers.json');
+const cart = require('./data/cart.json');
+
+module.exports = {
+    db,
+    products,signupUsers,cart
+}
